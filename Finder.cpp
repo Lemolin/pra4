10930ddf229c5d6b924900d721eea8fce2dc1cac
Lemolin <100899112+Lemolin@users.noreply.github.com>
2023-04-03 09:32:19 +0930

    Update Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -1,33 +1,31 @@
 #include "Finder.h"
 
-    using namespace std;
+using namespace std;
 
-    vector<int> Finder::findSubstrings(string s1, string s2) {
-
-        vector<int> result;
-        string substring;
-
-        for(size_t i = 1; i <= s2.size(); i++) {
-            if(substring.length()==0){
-                
-            substring += s2[i - 1];
-            }
-            
-            else{
-                substring += ' '+s2[i - 1];
-            }
-            size_t found = s1.find(substring);
-            if (found != string::npos) {
+vector<int> Finder::findSubstrings(string s1, string s2)
+{
+    vector<int> result;
+    for (size_t i = 1; i <= s2.size(); i++){
+        size_t found = s1.find(s2.substr(0,i));
+        if (found != string::npos)
+        {
+            if (result.size() == 0) {
                 result.push_back(found);
-            } else {
-                for (size_t j = i; j < s2.size(); j++) {
-                    result.push_back(-1);
-
-                }
-                return result;
+                s1.erase(0, found);
+            }
+            else {
+                result.push_back(result[0]);
             }
         }
-        return result;
+        else
+        {
+            for(size_t i=result.size(); i<s2.size(); i++){
+                result.push_back(-1);
+            }
+            return result;
+        }
     }
+    return result;
+}
 
    
