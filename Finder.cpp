a0e2c0dbb3715f4f1a1238b7f6cdac58409567c1
Lemolin <100899112+Lemolin@users.noreply.github.com>
2023-04-03 09:50:22 +0930

    Update Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -5,11 +5,15 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2)
 {
     vector<int> result;
+    int memory_index = 0;
     for (size_t i = 1; i <= s2.size(); i++){
         size_t found = s1.find(s2.substr(0,i));
         if (found != string::npos)
         {
-             result.push_back(found);
+            s1.erase(0,found);
+            found = found + memory_index;
+            result.push_back(found);
+            memory_index = found;
         }
         else
         {
@@ -21,5 +25,6 @@ vector<int> Finder::findSubstrings(string s1, string s2)
     }
     return result;
 }
+
 
    
